f7ff47c5c46b2f17d65463ae7cd38926fc6811e5
2166073 <133201193+2166073@users.noreply.github.com>
2023-12-01 11:13:00 +0100

    Add files via upload

--- db.php
+++ db.php
@@ -5,7 +5,7 @@ class Database{
     public function __construct($db = "test", $host = "localhost",  $user = "root", $pass = "Rasool123!") {
 
         try{
-            $this->pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+            $this->pdo = new PDO("mysql:$host=;dbname=$db", $user, $pass);
             $this->pdo->setAttribute(PDO:: ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             echo"connected to database $db";
            }catch(PDOException $e){
@@ -13,8 +13,32 @@ class Database{
            }  
         
         }
+       
+
+        public function insertUser($email, $password){
+           $stmt = $this->pdo->prepare("insert into user(email, password) values (?, ?)");
+           $password = password_hash($password, PASSWORD_DEFAULT);
+           $stmt->execute([$email, $password]);
+        }
+
+        public function select(){
+            $stmt = $this->pdo->query("SELECT * FROM user");
+            $resultaat = $stmt->fetchAll();
+            return $resultaat;
+        }
+
+        public function selectOneUser($id){
+            $stmt = $this->pdo->prepare("SELECT * FROM user WHERE id = ?"); 
+            $stmt->execute([$id]);
+            $resultaat = $stmt->fetch();
+            return $resultaat;
+        }
 }
 
-$connectie = new Database();                                            
+
+
+
+
+                                          
 
 ?>
--- home.php
+++ home.php
@@ -1,5 +1,45 @@
 <?php
 include ('db.php');
+$db = new Database(); 
+ 
 
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    try {
+        $db->insertUser($_POST['email'], $_POST['password']);
+        echo"Successfully added";
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    }
+}
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="POST">
+        <input type="text" name="email">
+        <input type="password" name="password">
+        <input type="submit">
+    </form>
+
+    <table border="2">
+        <tr>
+            <th>id</th>
+            <th>email</th>
+            <th>password</th>
+</tr>
+
+  <tr> <?php
+    $user = $db->selectOneUser(1); ?>
+    <td><?php echo $user['id'];?></td>
+    <td><?php echo $user['email'];?></td>
+    <td><?php echo $user['password'];?></td>
+    </tr>
+  </table>
+</body>
+</html>
